359843b8d4c09da75d0d0f364d4c0dee20961d95
周文王的追随者 <56502198+1255531455@users.noreply.github.com>
2021-09-25 16:13:13 +0800

    Delete matplotlibDemo directory

--- matplotlibDemo/SeabornDemo/test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = sns.load_dataset("penguins", data_home="C:\\Users\\yangdong\\PycharmProjects\\pythonProject\\venv\\Lib\\site"
-                                            "-packages\\seaborn\\seaborn_data")
-sns.pairplot(df, hue="species")
-plt.show()
-
